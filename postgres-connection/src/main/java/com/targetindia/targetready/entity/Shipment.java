aa9322a53a03ae040c73560551008e8e0635ff08
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-06-04 23:27:27 +0530

    Updated Location to OneToMany Mapping

--- postgres-connection/src/main/java/com/targetindia/targetready/entity/Shipment.java
+++ postgres-connection/src/main/java/com/targetindia/targetready/entity/Shipment.java
@@ -17,25 +17,27 @@ public class Shipment {
     @Id
     @Column(name = "id")
     private String shipment_id;
-    @Column(name = "shipping_method")
     private String shipping_method;
     @Column(name = "date")
     private String estimated_delivery_date;
     @Column(name = "cost")
     private Double total_cost;
-    @Column(name = "currency")
     private String currency;
-    @Column(name = "status")
     private String status;
 
-    @OneToOne(mappedBy = "shipment", cascade = CascadeType.ALL)
+    @JdbcTypeCode(SqlTypes.JSON)
     private Location sender;
-
-    @OneToOne(mappedBy = "shipment", cascade = CascadeType.ALL)
+    @JdbcTypeCode(SqlTypes.JSON)
     private Location recipient;
+   
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shpId", referencedColumnName = "id")
+    private List<Location> location = new ArrayList<>(List.of(sender, recipient));
 
 
-    @OneToMany(mappedBy = "shipment", cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shpId", referencedColumnName = "id")
     private List<Package> package_details;
 
 }
