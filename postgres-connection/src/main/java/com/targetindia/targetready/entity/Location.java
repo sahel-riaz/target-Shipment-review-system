7bbfeca21322aab0f4236156ffd902e401003a15
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-06-04 23:24:35 +0530

    Unidirectional Mapping

--- postgres-connection/src/main/java/com/targetindia/targetready/entity/Location.java
+++ postgres-connection/src/main/java/com/targetindia/targetready/entity/Location.java
@@ -12,25 +12,13 @@ import lombok.NoArgsConstructor;
 
 @Table(name = "location")
 public class Location {
-    @Id
+   @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "location_id")
     private Integer locationId;
-
-    @Column(name = "shipment_id")
-    private String shipmentId;
-    @Column(name = "identity")
-    private String identity;
-    @Column(name = "name")
+    private String identity; //Sender or Recipient
     private String name;
-    @Column(name = "address")
     private String address;
-    @Column(name = "contact")
     private String contact;
 
-
-
-    @OneToOne
-    @JoinColumn(name = "id")
-    private Shipment shipment;
 }
