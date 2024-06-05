5ef64f2e98b7118ec88f889314d15615f5a7fd23
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-06-06 02:24:20 +0530

    Avoided redundancy of shipment_id field

--- postgres-connection/src/main/java/com/targetindia/targetready/entity/Shipment.java
+++ postgres-connection/src/main/java/com/targetindia/targetready/entity/Shipment.java
@@ -4,7 +4,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 @Data
 @NoArgsConstructor
@@ -12,32 +16,40 @@ import java.util.List;
 @Entity
 @Table(name = "shipment")
 
-public class Shipment {
+public class Shipment implements Serializable {
+
 
     @Id
-    @Column(name = "id")
     private String shipment_id;
     private String shipping_method;
-    @Column(name = "date")
     private String estimated_delivery_date;
-    @Column(name = "cost")
     private Double total_cost;
     private String currency;
     private String status;
-
     @JdbcTypeCode(SqlTypes.JSON)
-    private Location sender;
+    private User sender;
     @JdbcTypeCode(SqlTypes.JSON)
-    private Location recipient;
-   
+    private User recipient;
 
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "shpId", referencedColumnName = "id")
-    private List<Location> location = new ArrayList<>(List.of(sender, recipient));
+    @JoinColumn(name = "shipment_id", referencedColumnName = "shipment_id")
+    private List<User> users = new ArrayList<>();
 
 
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "shpId", referencedColumnName = "id")
+    @JoinColumn(name = "shipment_id", referencedColumnName = "shipment_id")
     private List<Package> package_details;
 
+    @PrePersist
+    @PreUpdate
+    private void setUsers(){
+        if (sender != null){
+            sender.setIdentity("Sender");
+            this.users.add(this.sender);
+        }
+        if (recipient != null) {
+            recipient.setIdentity("Recipient");
+            this.users.add(this.recipient);
+        }
+    }
 }
