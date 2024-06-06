7f4b7e54b0f0a8a6b0f942b3a45f1c2906a3742f
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-06-06 14:52:49 +0530

    Delete src directory

--- src/main/java/com/target/shipments/repository/ShipmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.target.shipments.repository;
-
-import com.target.shipments.model.Shipment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShipmentRepository extends JpaRepository<Shipment, String> {
-}
--- src/main/java/com/target/shipments/repository/TestTableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.target.shipments.repository;
-
-import com.target.shipments.model.TestTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestTableRepository extends JpaRepository<TestTable, String> {
-}
