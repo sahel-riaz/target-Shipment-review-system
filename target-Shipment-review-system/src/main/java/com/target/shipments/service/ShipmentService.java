b12af6afbd42cec5d4b4da014e91a0b10e851d06
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-06-19 18:33:29 +0530

    Update Save method ShipmentService.java

--- target-Shipment-review-system/src/main/java/com/target/shipments/service/ShipmentService.java
+++ target-Shipment-review-system/src/main/java/com/target/shipments/service/ShipmentService.java
@@ -1,6 +1,8 @@
 package com.target.shipments.service;
 
+import com.target.shipments.model.Location;
 import com.target.shipments.model.Shipment;
+import com.target.shipments.repository.LocationRepository;
 import com.target.shipments.repository.ShipmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,9 @@ public class ShipmentService {
     @Autowired
     private ShipmentRepository shipmentRepository;
 
+    @Autowired
+    private LocationRepository locationRepository;
+
     public List<Shipment> getAll() {
         return shipmentRepository.findAll();
     }
@@ -21,7 +26,29 @@ public class ShipmentService {
     }
 
     public Shipment save(Shipment shipment) {
+
+        Location sender = shipment.getSender();
+        Location recipient = shipment.getRecipient();
+
+        sender.setIdentity("Sender");
+        recipient.setIdentity("Recipient");
+        Location findSender = locationRepository.findLocation(sender.getName(), sender.getIdentity(),
+                                                                sender.getAddress(), sender.getContact());
+        Location findRecipient = locationRepository.findLocation(recipient.getName(), recipient.getIdentity(),
+                                                                recipient.getAddress(), recipient.getContact());
+        
+        if(findRecipient != null && findSender != null){
+            shipment.setRecipient(findRecipient);
+            shipment.setSender(findSender);
+        }
+        else if(findSender == null){
+            shipment.setRecipient(findRecipient);
+        }
+        else {
+            shipment.setSender(findSender);
+        }
         return shipmentRepository.save(shipment);
+
     }
 
     public void deleteById(String id) {
