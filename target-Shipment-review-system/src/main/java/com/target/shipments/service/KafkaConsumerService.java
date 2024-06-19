d25fc2935632a6c5ebb582a3fd816d554c09e491
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-06-19 16:23:18 +0530

    Update KafkaConsumerService.java

--- target-Shipment-review-system/src/main/java/com/target/shipments/service/KafkaConsumerService.java
+++ target-Shipment-review-system/src/main/java/com/target/shipments/service/KafkaConsumerService.java
@@ -1,35 +1,9 @@
-// package com.target.shipments.service;
-
-// import com.fasterxml.jackson.databind.ObjectMapper;
-// import com.target.shipments.model.Shipment;
-// import com.target.shipments.repository.ShipmentRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.kafka.annotation.KafkaListener;
-// import org.springframework.stereotype.Service;
-
-// @Service
-// public class KafkaConsumerService {
-//     @Autowired
-//     private ShipmentRepository shipmentRepository;
-
-//     private final ObjectMapper objectMapper = new ObjectMapper();
-
-//     @KafkaListener(topics = "shipments_topic", groupId = "com.target")
-//     public void consume(String message) {
-//         try {
-//             Shipment shipment = objectMapper.readValue(message, Shipment.class);
-//             shipmentRepository.save(shipment);
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         }
-//     }
-// }
 
 package com.target.shipments.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.target.shipments.model.TestTable;
-import com.target.shipments.repository.TestTableRepository;
+import com.target.shipments.model.Shipment;
+import com.target.shipments.repository.ShipmentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +16,7 @@ public class KafkaConsumerService {
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
 
     @Autowired
-    private TestTableRepository testTableRepository;
+    private ShipmentRepository shipmentRepository;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -50,10 +24,10 @@ public class KafkaConsumerService {
     public void consume(String message) {
         logger.info("Received message: {}", message);
         try {
-            TestTable testTable = objectMapper.readValue(message, TestTable.class);
-            if (testTable.getTestId() != null) {  
-                testTableRepository.save(testTable);
-                logger.info("Message consumed and saved: {}", testTable);
+            Shipment shipment = objectMapper.readValue(message, Shipment.class);
+            if (shipment.getShipment_id() != null) {
+                shipmentRepository.save(shipment);
+                logger.info("Message consumed and saved: {}", shipment);
             } else {
                 logger.warn("testId is null, skipping save.");
             }
