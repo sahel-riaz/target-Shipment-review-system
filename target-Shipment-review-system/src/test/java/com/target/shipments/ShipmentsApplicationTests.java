4234d688cb9b4f22319bf03d29c1a9dc44fd7890
sahelriaz <sahelnriaz@gmail.com>
2024-07-04 13:26:47 +0400

    ES Integrated

---
+++ target-Shipment-review-system/src/test/java/com/target/shipments/ShipmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.target.shipments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
