25a067385321da1e99ca0e7e4fc8d623b0baa4e4
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-07-08 04:35:05 +0530

    Update ShipmentRepository.java

--- target-Shipment-review-system/src/main/java/com/target/shipments/repository/ShipmentRepository.java
+++ target-Shipment-review-system/src/main/java/com/target/shipments/repository/ShipmentRepository.java
@@ -3,5 +3,21 @@ package com.target.shipments.repository;
 import com.target.shipments.model.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
+@EnableJpaRepositories
 public interface ShipmentRepository extends JpaRepository<Shipment, String> {
+
+
+    List<Shipment> findByStatus(String status);
+    long countByStatus(String status);
+
+    @Query("Select s from Shipment s where s.shipping_method = ?1")
+    List<Shipment> findByShipping_Method(String shipping_method);
+    @Query("Select count(*) from Shipment s where s.shipping_method = ?1")
+
+    long countByShipping_Method(String shipping_method);
+
+    List<Shipment> findByCurrency(String currency);
+    long countByCurrency(String currency);
+
 }
