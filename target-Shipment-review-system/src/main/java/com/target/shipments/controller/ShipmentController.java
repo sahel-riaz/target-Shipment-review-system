b6233d5a653ffc128ffee49ea8dec527658e0733
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2024-07-08 04:30:45 +0530

    Update ShipmentController.java

--- target-Shipment-review-system/src/main/java/com/target/shipments/controller/ShipmentController.java
+++ target-Shipment-review-system/src/main/java/com/target/shipments/controller/ShipmentController.java
@@ -15,6 +15,8 @@ public class ShipmentController {
     @Autowired
     private ShipmentService shipmentService;
 
+
+    //GET Requests
     @GetMapping
     public List<Shipment> getAll() {
         return shipmentService.getAll();
@@ -30,14 +32,51 @@ public class ShipmentController {
         }
     }
 
+    @GetMapping("/status")
+    public List<Shipment> getShipmentsByStatus(@RequestParam String status) {
+        return shipmentService.getByStatus(status);
+    }
+
+    @GetMapping("/currency")
+    public List<Shipment> getShipmentsByCurrency(@RequestParam String currency) {
+        return shipmentService.getByCurrency(currency);
+    }
+
+    @GetMapping("/shipping_method")
+    public List<Shipment> getShipmenByShpMethod(@RequestParam String shipping_method) {
+        return shipmentService.getByShippingMethod(shipping_method);
+    }
+
+
+    @GetMapping("/count/status")
+    public long getShipmentStatusCount(@RequestParam String status) {
+        return shipmentService.getStatusCount(status);
+    }
+
+    @GetMapping("/count/currency")
+    public long getShipmentCurrencyCount(@RequestParam String currency) {
+        return shipmentService.getCurrencyCount(currency);
+    }
+
+    @GetMapping("/count/shipping_method")
+    public long getShipmenShpMethodCount(@RequestParam String shipping_method) {
+        return shipmentService.getShippingMethodCount(shipping_method);
+    }
+
+
+
+//POST Requests
     @PostMapping
     public Shipment create(@RequestBody Shipment shipment) {
         return shipmentService.save(shipment);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Shipment> update(@PathVariable String id, @RequestBody Shipment shipment) {
+
+    //PUT Requests
+    @PutMapping ("/{id}")
+    public ResponseEntity<Shipment> update(@PathVariable String id) {
         if (shipmentService.getById(id).isPresent()) {
+            Shipment shipment = shipmentService.getById(id).get();
             shipment.setShipment_id(id);
             return ResponseEntity.ok(shipmentService.save(shipment));
         } else {
@@ -45,6 +84,41 @@ public class ShipmentController {
         }
     }
 
+    @PutMapping ("/{id}/shipping_method/{shipping_method}")
+    public ResponseEntity<Shipment> updateShippingMethod(@PathVariable String id, @PathVariable String shipping_method) {
+        if (shipmentService.getById(id).isPresent()) {
+            Shipment shipment = shipmentService.getById(id).get();
+            shipment.setShipping_method(shipping_method);
+            return ResponseEntity.ok(shipmentService.save(shipment));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @PutMapping ("/{id}/currency/{currency}")
+    public ResponseEntity<Shipment> updateCurrency(@PathVariable String id, @PathVariable String currency) {
+        if (shipmentService.getById(id).isPresent()) {
+            Shipment shipment = shipmentService.getById(id).get();
+            shipment.setCurrency(currency);
+            return ResponseEntity.ok(shipmentService.save(shipment));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping ("/{id}/status/{status}")
+    public ResponseEntity<Shipment> updateStatus(@PathVariable String id, @PathVariable String status) {
+        if (shipmentService.getById(id).isPresent()) {
+            Shipment shipment = shipmentService.getById(id).get();
+            shipment.setStatus(status);
+            return ResponseEntity.ok(shipmentService.save(shipment));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+//DELETE Requests
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
         if (shipmentService.getById(id).isPresent()) {
